e28cdce2ae7095cecc3d556d143f333e77fd0ead
Killian Lopez <Killian.Lopez@outlook.com>
2025-09-29 18:02:01 +0200

    chore(build): delete "Feca SUPPORT [ARMURE] 110 low Cost"

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -16,220 +16,213 @@ const CLASS_LIST = {
 };
 
 const BUILD_DATA = {
-    "Huppermage DPT [MELEE] 245 Opti": {
-        "image": "assets/builds/Huppermage DPT.png",
-        "url": "https://zenithwakfu.com/builder/pwssw",
-        "keywords": ["Opti"],
-        "class": "Huppermage",
-        "description": "Gameplay Principalement mélée - Terre",
-        "tranche": 245
-        }
-    ,
-    "Ecaflip SUPPORT [PUCIF] 245 Opti": {
-        "image": "assets/builds/Ecaflip SUPPORT.png",
-        "url": "https://zenithwakfu.com/builder/1tk28",
-        "keywords": ["Opti"],
-        "class": "Ecaflip",
-        "description": "Gameplay basé autours de PUCIF de l'ecaflip !",
-        "tranche": 245
-    }
-    ,
-    "Enutrof SUPPORT [ABANDON] 245 Opti": {
-        "image": "assets/builds/Enutrof SUPPORT.png",
-        "url": "https://zenithwakfu.com/builder/ctzuw",
-        "keywords": ["Opti"],
-        "class": "Enutrof",
-        "description": "Gameplay basé autours du retrait résistance et retrait PM",
-        "tranche": 245
-    }
-    ,
-    "Cra DPT [TRI-ELEMENT] 245 Opti": {
-        "image": "assets/builds/Cra DPT.png",
-        "url": "https://zenithwakfu.com/builder/nhv8j",
-        "keywords": ["Opti"],
-        "class": "Cra",
-        "description": "Gameplay TRI-ELEMENT autours de Elementalisme !",
-        "tranche": 245
-    }
-    ,
-    "Xelor DPT [EAU FEU] 245 Opti": {
-        "image": "assets/builds/Xelor DPT [Eau Feu].png",
-        "url": "https://zenithwakfu.com/builder/onndc",
-        "keywords": ["Opti"],
-        "class": "Xelor",
-        "description": "Build Classique Xelor DPT",
-        "tranche": 245
-    }
-,
-    "Osamodas SUPPORT [ARMURE] 245 Opti": {
-    "image": "assets/builds/Osamodas SUPPORT [Shield].png",
-    "url": "https://zenithwakfu.com/builder/y9ayo",
-    "keywords": ["Opti"],
-    "class": "Osamodas",
-    "description": "Gameplay base autours du shield grâce au item %armure donnée de la tranche !",
-    "tranche": 245
-    }
-,
-    "Iop DPT [FEU TERRE] 245 Opti": {
-    "image": "assets/builds/Iop DPT[Feu Terre].png",
-    "url": "https://zenithwakfu.com/builder/76cko",
-    "keywords": ["Opti"],
-    "class": "Iop",
-    "description": "Gameplay Feu-Terre Spé Zoning et clear de salle !",
-    "tranche": 245
-    }
-,
-    "Eniripsa SUPPORT [SOIN ARMURE] 245 Opti": {
-    "image": "assets/builds/Eniripsa SUPPORT [Soin Shield].png",
-    "url": "https://zenithwakfu.com/builder/tjbqt",
-    "keywords": ["Opti"],
-    "class": "Eniripsa",
-    "description": "Gameplay autours de Distribution maxant l'amure et le soin réalisé ! ",
-    "tranche": 245
-    }
-,
-"Pandawa SUPPORT [PLACEUR SOIN] 245 Opti": {
-    "image": "assets/builds/Pandawa SUPPORT [Placeur Soin].png",
-    "url": "https://zenithwakfu.com/builder/xrrmc",
-    "keywords": ["Opti"],
-    "class": "Pandawa",
-    "description": "Gameplay permettant un soin d'appoint non négligeable avec le Panda les tours où il n'a pas besoin de placer.",
-    "tranche": 245
-    }
-,
-    "Steamer DPT [DIRECT] 110 Opti": {
-    "image": "assets/builds/Steamer DPT [Direct].png",
-    "url": "https://zenithwakfu.com/builder/9jvjn",
-    "keywords": ["Opti"],
-    "class": "Steamer",
-    "description": "",
-    "tranche": 110
-    }
-,
-    "Enutrof SUPPORT [ABANDON] 110 Low Cost": {
-"image": "assets/builds/Enutrof SUPPORT [ABANDON] 110.png",
-"url": "https://www.zenithwakfu.com/builder/cd686",
-"keywords": ["Low Cost"],
-"class": "Enutrof",
-"description": "Le stuff n'a pas les subli abandon mais il est prêt pour en recevoir et être facile à améliorer !",
-"tranche": 110
-},
-    "Osamodas SUPPORT [ABANDON] 110 Opti": {
-    "image": "assets/builds/Osamodas SUPPORT [Abandon].png",
-    "url": "https://www.zenithwakfu.com/builder/416c3",
-    "keywords": ["Opti"],
-    "class": "Osamodas",
-    "description": "",
-    "tranche": 110
-    }
-,
-    "Sadida DPT [POISON POUPEE] 110 Opti": {
-    "image": "assets/builds/Sadida DPT [POISON + POUPEE].png",
-    "url": "https://zenithwakfu.com/builder/32e98",
-    "keywords": ["Opti"],
-    "class": "Sadida",
-    "description": "Necessite d'avoir de quoi abuser des retraits PA/PM dans votre composition pour profiter du Sadi !",
-    "tranche": 110
-},
-
-"Ouginak DPT [DIRECT] 110 Mid Cost": {
-"image": "assets/builds/Ouginak DPT [Direct] 110.png",
-"url": "https://zenithwakfu.com/builder/0ruzb",
-"keywords": ["Mid Cost"],
-"class": "Ouginak",
-"description": "Croc futé convertie auto la parade en CC quand tu es en Ougi garou, donc avec ce build vous avez 94%cc dès que vous êtes transformé. J'ai mis des sauvegardes car régulierement elles sont en dessous des 500.000/u ",
-"tranche": 110
-}
-,
-
-"Sadida SUPPORT [SOIN] 110 Mid Cost": {
-"image": "assets/builds/Sadida SUPPORT [Soin] 110.png",
-"url": "https://www.zenithwakfu.com/builder/91980",
-"keywords": ["Mid Cost"],
-"class": "Sadida",
-"description": "",
-"tranche": 110
-}
-,
-"Eniripsa SUPPORT [SOIN] 110 Low Cost": {
-"image": "assets/builds/Eniripsa SUPPORT [SOIN] 110.png",
-"url": "https://www.zenithwakfu.com/builder/c86e0",
-"keywords": ["Low Cost"],
-"class": "Eniripsa",
-"description": "",
-"tranche": 110
-},
-"Sacrieur DPT [MELEE BERSERK] 110 Low Cost": {
-"image": "assets/builds/Sacrieur DPT [MELEE BERSERK] 110.png",
-"url": "https://www.zenithwakfu.com/builder/62edf",
-"keywords": ["Low Cost"],
-"class": "Sacrieur",
-"description": "",
-"tranche": 110
-}
-,
-"Eniripsa SUPPORT [SOIN] 110 Mid Cost": {
-"image": "assets/builds/Eniripsa SUPP [SOIN] 110.png",
-"url": "https://www.zenithwakfu.com/builder/xd9bf",
-"keywords": ["Mid Cost"],
-"class": "Eniripsa",
-"description": "Vous pouvez mettre un Royal Mimint si vous préferez gagner un PO au détriment d'un PA. Un familier maitrise soin est l'idéal, mais celui-ci fera l'affaire le temps de vous procurer mieux !",
-"tranche": 110
-}
-,
-"Steamer DPT [DISTANCE] 110 Mid Cost": {
-"image": "assets/builds/Steamer DPT [DISTANCE] 110.png",
-"url": "https://zenithwakfu.com/builder/nn88y",
-"keywords": ["Mid Cost"],
-"class": "Steamer",
-"description": "",
-"tranche": 110
-}
-,
-"Ecaflip SUPPORT [SOIN BOOST] 110 Opti": {
-"image": "assets/builds/Ecaflip SUPPORT [SOIN BOOST] 110.png",
-"url": "https://www.zenithwakfu.com/builder/ssnhy",
-"keywords": ["Opti"],
-"class": "Ecaflip",
-"description": "",
-"tranche": 110
-}
-,
-"Feca SUPPORT [ABANDON] 110 Opti": {
-"image": "assets/builds/Feca SUPPORT [ABANDON] 110.png",
-"url": "https://www.zenithwakfu.com/builder/f9a4d",
-"keywords": ["Opti"],
-"class": "Feca",
-"description": "Nécessite de viser les 84% de parade pour le combo PAISIBLE !",
-"tranche": 110
-}
-,
-"Xelor DPT [EAU FEU] 110 Mid Cost": {
-"image": "assets/builds/Xelor DPT [EAU FEU] 110.png",
-"url": "https://www.zenithwakfu.com/builder/d4e5a",
-"keywords": ["Mid Cost"],
-"class": "Xelor",
-"description": "",
-"tranche": 110
-}
-,
-  "Osa SUPPORT [ARMURE] Low cost": {
-    "image": "assets/builds/Osa SUPPORT [ARMURE] Low cost.png",
-    "url": "https://www.zenithwakfu.com/builder/0463f",
-    "keywords": ["Low Cost"],
-    "class": "Osamodas",
-    "description": "",
-    "tranche": 110
+  
+      "Huppermage DPT [MELEE] 245 Opti": {
+          "image": "assets/builds/Huppermage DPT.png",
+          "url": "https://zenithwakfu.com/builder/pwssw",
+          "keywords": ["Opti"],
+          "class": "Huppermage",
+          "description": "Gameplay Principalement mélée - Terre",
+          "tranche": 245
+          }
+      ,
+      "Ecaflip SUPPORT [PUCIF] 245 Opti": {
+          "image": "assets/builds/Ecaflip SUPPORT.png",
+          "url": "https://zenithwakfu.com/builder/1tk28",
+          "keywords": ["Opti"],
+          "class": "Ecaflip",
+          "description": "Gameplay basé autours de PUCIF de l'ecaflip !",
+          "tranche": 245
+      }
+      ,
+      "Enutrof SUPPORT [ABANDON] 245 Opti": {
+          "image": "assets/builds/Enutrof SUPPORT.png",
+          "url": "https://zenithwakfu.com/builder/ctzuw",
+          "keywords": ["Opti"],
+          "class": "Enutrof",
+          "description": "Gameplay basé autours du retrait résistance et retrait PM",
+          "tranche": 245
+      }
+      ,
+      "Cra DPT [TRI-ELEMENT] 245 Opti": {
+          "image": "assets/builds/Cra DPT.png",
+          "url": "https://zenithwakfu.com/builder/nhv8j",
+          "keywords": ["Opti"],
+          "class": "Cra",
+          "description": "Gameplay TRI-ELEMENT autours de Elementalisme !",
+          "tranche": 245
+      }
+      ,
+      "Xelor DPT [EAU FEU] 245 Opti": {
+          "image": "assets/builds/Xelor DPT [Eau Feu].png",
+          "url": "https://zenithwakfu.com/builder/onndc",
+          "keywords": ["Opti"],
+          "class": "Xelor",
+          "description": "Build Classique Xelor DPT",
+          "tranche": 245
+      }
+  ,
+      "Osamodas SUPPORT [ARMURE] 245 Opti": {
+      "image": "assets/builds/Osamodas SUPPORT [Shield].png",
+      "url": "https://zenithwakfu.com/builder/y9ayo",
+      "keywords": ["Opti"],
+      "class": "Osamodas",
+      "description": "Gameplay base autours du shield grâce au item %armure donnée de la tranche !",
+      "tranche": 245
+      }
+  ,
+      "Iop DPT [FEU TERRE] 245 Opti": {
+      "image": "assets/builds/Iop DPT[Feu Terre].png",
+      "url": "https://zenithwakfu.com/builder/76cko",
+      "keywords": ["Opti"],
+      "class": "Iop",
+      "description": "Gameplay Feu-Terre Spé Zoning et clear de salle !",
+      "tranche": 245
+      }
+  ,
+      "Eniripsa SUPPORT [SOIN ARMURE] 245 Opti": {
+      "image": "assets/builds/Eniripsa SUPPORT [Soin Shield].png",
+      "url": "https://zenithwakfu.com/builder/tjbqt",
+      "keywords": ["Opti"],
+      "class": "Eniripsa",
+      "description": "Gameplay autours de Distribution maxant l'amure et le soin réalisé ! ",
+      "tranche": 245
+      }
+  ,
+  "Pandawa SUPPORT [PLACEUR SOIN] 245 Opti": {
+      "image": "assets/builds/Pandawa SUPPORT [Placeur Soin].png",
+      "url": "https://zenithwakfu.com/builder/xrrmc",
+      "keywords": ["Opti"],
+      "class": "Pandawa",
+      "description": "Gameplay permettant un soin d'appoint non négligeable avec le Panda les tours où il n'a pas besoin de placer.",
+      "tranche": 245
+      }
+  ,
+      "Steamer DPT [DIRECT] 110 Opti": {
+      "image": "assets/builds/Steamer DPT [Direct].png",
+      "url": "https://zenithwakfu.com/builder/9jvjn",
+      "keywords": ["Opti"],
+      "class": "Steamer",
+      "description": "",
+      "tranche": 110
+      }
+  ,
+      "Enutrof SUPPORT [ABANDON] 110 Low Cost": {
+  "image": "assets/builds/Enutrof SUPPORT [ABANDON] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/cd686",
+  "keywords": ["Low Cost"],
+  "class": "Enutrof",
+  "description": "Le stuff n'a pas les subli abandon mais il est prêt pour en recevoir et être facile à améliorer !",
+  "tranche": 110
+  },
+      "Osamodas SUPPORT [ABANDON] 110 Opti": {
+      "image": "assets/builds/Osamodas SUPPORT [Abandon].png",
+      "url": "https://www.zenithwakfu.com/builder/416c3",
+      "keywords": ["Opti"],
+      "class": "Osamodas",
+      "description": "",
+      "tranche": 110
+      }
+  ,
+      "Sadida DPT [POISON POUPEE] 110 Opti": {
+      "image": "assets/builds/Sadida DPT [POISON + POUPEE].png",
+      "url": "https://zenithwakfu.com/builder/32e98",
+      "keywords": ["Opti"],
+      "class": "Sadida",
+      "description": "Necessite d'avoir de quoi abuser des retraits PA/PM dans votre composition pour profiter du Sadi !",
+      "tranche": 110
+  },
+  
+  "Ouginak DPT [DIRECT] 110 Mid Cost": {
+  "image": "assets/builds/Ouginak DPT [Direct] 110.png",
+  "url": "https://zenithwakfu.com/builder/0ruzb",
+  "keywords": ["Mid Cost"],
+  "class": "Ouginak",
+  "description": "Croc futé convertie auto la parade en CC quand tu es en Ougi garou, donc avec ce build vous avez 94%cc dès que vous êtes transformé. J'ai mis des sauvegardes car régulierement elles sont en dessous des 500.000/u ",
+  "tranche": 110
   }
-,
-  "Feca SUPPORT [ARMURE] 110 low Cost": {
-    "image": "assets/builds/Feca SUPPORT [ARMURE] 110 low Cost.png",
-    "url": "https://www.zenithwakfu.com/builder/a9180",
-    "keywords": ["Low Cost"],
-    "class": "Feca",
-    "description": "",
-    "tranche": 110
+  ,
+  
+  "Sadida SUPPORT [SOIN] 110 Mid Cost": {
+  "image": "assets/builds/Sadida SUPPORT [Soin] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/91980",
+  "keywords": ["Mid Cost"],
+  "class": "Sadida",
+  "description": "",
+  "tranche": 110
   }
+  ,
+  "Eniripsa SUPPORT [SOIN] 110 Low Cost": {
+  "image": "assets/builds/Eniripsa SUPPORT [SOIN] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/c86e0",
+  "keywords": ["Low Cost"],
+  "class": "Eniripsa",
+  "description": "",
+  "tranche": 110
+  },
+  "Sacrieur DPT [MELEE BERSERK] 110 Low Cost": {
+  "image": "assets/builds/Sacrieur DPT [MELEE BERSERK] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/62edf",
+  "keywords": ["Low Cost"],
+  "class": "Sacrieur",
+  "description": "",
+  "tranche": 110
+  }
+  ,
+  "Eniripsa SUPPORT [SOIN] 110 Mid Cost": {
+  "image": "assets/builds/Eniripsa SUPP [SOIN] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/xd9bf",
+  "keywords": ["Mid Cost"],
+  "class": "Eniripsa",
+  "description": "Vous pouvez mettre un Royal Mimint si vous préferez gagner un PO au détriment d'un PA. Un familier maitrise soin est l'idéal, mais celui-ci fera l'affaire le temps de vous procurer mieux !",
+  "tranche": 110
+  }
+  ,
+  "Steamer DPT [DISTANCE] 110 Mid Cost": {
+  "image": "assets/builds/Steamer DPT [DISTANCE] 110.png",
+  "url": "https://zenithwakfu.com/builder/nn88y",
+  "keywords": ["Mid Cost"],
+  "class": "Steamer",
+  "description": "",
+  "tranche": 110
+  }
+  ,
+  "Ecaflip SUPPORT [SOIN BOOST] 110 Opti": {
+  "image": "assets/builds/Ecaflip SUPPORT [SOIN BOOST] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/ssnhy",
+  "keywords": ["Opti"],
+  "class": "Ecaflip",
+  "description": "",
+  "tranche": 110
+  }
+  ,
+  "Feca SUPPORT [ABANDON] 110 Opti": {
+  "image": "assets/builds/Feca SUPPORT [ABANDON] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/f9a4d",
+  "keywords": ["Opti"],
+  "class": "Feca",
+  "description": "Nécessite de viser les 84% de parade pour le combo PAISIBLE !",
+  "tranche": 110
+  }
+  ,
+  "Xelor DPT [EAU FEU] 110 Mid Cost": {
+  "image": "assets/builds/Xelor DPT [EAU FEU] 110.png",
+  "url": "https://www.zenithwakfu.com/builder/d4e5a",
+  "keywords": ["Mid Cost"],
+  "class": "Xelor",
+  "description": "",
+  "tranche": 110
+  }
+  ,
+    "Osa SUPPORT [ARMURE] Low cost": {
+      "image": "assets/builds/Osa SUPPORT [ARMURE] Low cost.png",
+      "url": "https://www.zenithwakfu.com/builder/0463f",
+      "keywords": ["Low Cost"],
+      "class": "Osamodas",
+      "description": "",
+      "tranche": 110
+    }
+  ,
 };
 
 export { BUILD_DATA, CLASS_LIST };
