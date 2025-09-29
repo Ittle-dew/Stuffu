994d2cb37a042aef6f18afdca1ae66cdeddaadf7
Killian Lopez <Killian.Lopez@outlook.com>
2025-09-29 14:46:42 +0200

    chore(build): delete "Enutrof SUPPORT"

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -16,24 +16,14 @@ const CLASS_LIST = {
 };
 
 const BUILD_DATA = {
-  
-    "Enutrof SUPPORT": {
-        "image": "assets/builds/Enutrof SUPPORT.png",
-        "url": "https://zenithwakfu.com/builder/ctzuw",
-        "keywords": ["Opti"],
-        "class": "Enutrof",
-        "description": "Stuff Spé retrait Résistance/Retrait PM",
-        "tranche": 245
-      }
-  ,
   "Crâ DPT": {
-    "image": "assets/builds/Crâ DPT.png",
-    "url": "https://zenithwakfu.com/builder/1tk28",
-    "keywords": ["Opti"],
-    "class": "Cra",
-    "description": "Stuff \"Elementalisme\" Tri-Element",
-    "tranche": 245
-  },
+      "image": "assets/builds/Crâ DPT.png",
+      "url": "https://zenithwakfu.com/builder/1tk28",
+      "keywords": ["Opti"],
+      "class": "Cra",
+      "description": "Stuff \"Elementalisme\" Tri-Element",
+      "tranche": 245
+    },
 };
 
 export { BUILD_DATA, CLASS_LIST };
