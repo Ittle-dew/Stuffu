8c7ccd8ab1b98ec3c79a91d2cf3e97122c9eb02b
Killian Lopez <Killian.Lopez@outlook.com>
2025-09-29 15:21:35 +0200

    chore(build): delete "Osamodas SUPPORT [Shield]"

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -16,58 +16,51 @@ const CLASS_LIST = {
 };
 
 const BUILD_DATA = {
-  "Huppermage DPT": {
-      "image": "assets/builds/Huppermage DPT.png",
-      "url": "https://zenithwakfu.com/builder/pwssw",
+  
+    "Huppermage DPT": {
+        "image": "assets/builds/Huppermage DPT.png",
+        "url": "https://zenithwakfu.com/builder/pwssw",
+        "keywords": ["Opti"],
+        "class": "Huppermage",
+        "description": "Gameplay Principalement mélée - Terre",
+        "tranche": 245
+      }
+  ,
+    "Ecaflip SUPPORT": {
+      "image": "assets/builds/Ecaflip SUPPORT.png",
+      "url": "https://zenithwakfu.com/builder/1tk28",
       "keywords": ["Opti"],
-      "class": "Huppermage",
-      "description": "Gameplay Principalement mélée - Terre",
+      "class": "Ecaflip",
+      "description": "Gameplay basé autours de PUCIF de l'ecaflip !",
       "tranche": 245
     }
-,
-  "Ecaflip SUPPORT": {
-    "image": "assets/builds/Ecaflip SUPPORT.png",
-    "url": "https://zenithwakfu.com/builder/1tk28",
-    "keywords": ["Opti"],
-    "class": "Ecaflip",
-    "description": "Gameplay basé autours de PUCIF de l'ecaflip !",
-    "tranche": 245
-  }
-,
-  "Enutrof SUPPORT": {
-    "image": "assets/builds/Enutrof SUPPORT.png",
-    "url": "https://zenithwakfu.com/builder/ctzuw",
-    "keywords": ["Opti"],
-    "class": "Enutrof",
-    "description": "Gameplay basé autours du retrait résistance et retrait PM",
-    "tranche": 245
-  }
-,
-  "Cra DPT": {
-    "image": "assets/builds/Cra DPT.png",
-    "url": "https://zenithwakfu.com/builder/nhv8j",
-    "keywords": ["Opti"],
-    "class": "Cra",
-    "description": "Gameplay TRI-ELEMENT autours de Elementalisme !",
-    "tranche": 245
-  }
-,
-  "Osamodas SUPPORT [Shield]": {
-  "image": "assets/builds/Osamodas SUPPORT [Shield].png",
-  "url": "https://zenithwakfu.com/builder/y9ayo",
-  "keywords": ["Opti"],
-  "class": "Xelor",
-  "description": "Gameplay base autours du shield grâce au item %armure donnée de la tranche !",
-  "tranche": 245
-},
+  ,
+    "Enutrof SUPPORT": {
+      "image": "assets/builds/Enutrof SUPPORT.png",
+      "url": "https://zenithwakfu.com/builder/ctzuw",
+      "keywords": ["Opti"],
+      "class": "Enutrof",
+      "description": "Gameplay basé autours du retrait résistance et retrait PM",
+      "tranche": 245
+    }
+  ,
+    "Cra DPT": {
+      "image": "assets/builds/Cra DPT.png",
+      "url": "https://zenithwakfu.com/builder/nhv8j",
+      "keywords": ["Opti"],
+      "class": "Cra",
+      "description": "Gameplay TRI-ELEMENT autours de Elementalisme !",
+      "tranche": 245
+    }
+  ,
   "Xelor DPT [Eau Feu]": {
-    "image": "assets/builds/Xelor DPT [Eau Feu].png",
-    "url": "https://zenithwakfu.com/builder/onndc",
-    "keywords": ["Opti"],
-    "class": "Xelor",
-    "description": "Build Classique Xelor DPT",
-    "tranche": 245
-  }
+      "image": "assets/builds/Xelor DPT [Eau Feu].png",
+      "url": "https://zenithwakfu.com/builder/onndc",
+      "keywords": ["Opti"],
+      "class": "Xelor",
+      "description": "Build Classique Xelor DPT",
+      "tranche": 245
+    }
 };
 
 export { BUILD_DATA, CLASS_LIST };
