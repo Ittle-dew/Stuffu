c263798b72339019d491e44c9303b8240998429d
Killian Lopez <Killian.Lopez@outlook.com>
2025-09-30 12:29:31 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -1623,6 +1623,15 @@ const BUILD_DATA = {
     "description": "Hupper principalement maîtrise elem avec un peu de mêlée/crit, le but est d'avoir 100% CC tout seul et de se jouer avec le gameplay Constant Terre, via les Passifs Extensions des Sens et Transcendance Runique",
     "tranche": 200
   }
+,
+  "Huppermage DPT [NEUTRALITE] OPTI": {
+    "image": "assets/builds/Huppermage DPT [NEUTRALITE] OPTI.png",
+    "url": "https://www.zenithwakfu.com/builder/b5e8b",
+    "keywords": ["Opti"],
+    "class": "Huppermage",
+    "description": "",
+    "tranche": 200
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
