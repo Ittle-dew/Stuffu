af29b4907778525b9d83fd120f4f5b9940f97774
Killian Lopez <Killian.Lopez@outlook.com>
2025-09-30 11:31:38 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -1173,6 +1173,15 @@ const BUILD_DATA = {
     "description": "",
     "tranche": 200
   }
+,
+  "Enutrof DPT [MELEE] 200 Low Cost": {
+    "image": "assets/builds/Enutrof DPT [MELEE] 200 Low Cost.png",
+    "url": "https://zenithwakfu.com/builder/eb294",
+    "keywords": ["Low Cost"],
+    "class": "Enutrof",
+    "description": "",
+    "tranche": 200
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
