64f91b4ddb50d554bcabf3bb10a02ea804aad1dc
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-06 13:56:48 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -1953,6 +1953,15 @@ const BUILD_DATA = {
     "description": "Fait par Shodan ",
     "tranche": 20
   }
+,
+  "Iop DPT [MELEE] 20 low cost": {
+    "image": "assets/builds/Iop DPT [MELEE] 20 low cost.png",
+    "url": "https://www.zenithwakfu.com/builder/irzw0",
+    "keywords": ["Low Cost"],
+    "class": "Iop",
+    "description": "Fait par Shodan ",
+    "tranche": 20
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
