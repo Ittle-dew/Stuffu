94a4c539debd91253ed4191b36ac37954a72dd4e
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-06 14:31:09 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -2162,6 +2162,15 @@ const BUILD_DATA = {
     "description": "",
     "tranche": 140
   }
+,
+  "Sadida SUPPORT [SOIN PLACEUR] 140 Low cost": {
+    "image": "assets/builds/Sadida SUPPORT [SOIN PLACEUR] 140 Low cost.png",
+    "url": "https://zenithwakfu.com/builder/ykazy",
+    "keywords": ["Low Cost"],
+    "class": "Sadida",
+    "description": "",
+    "tranche": 140
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
