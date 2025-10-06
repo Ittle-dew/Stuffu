2eed68141d22e85ea2060974057cb9df78169f9e
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-06 14:00:02 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -2027,6 +2027,15 @@ const BUILD_DATA = {
     "description": "Fait par Shodan ",
     "tranche": 20
   }
+,
+  "Cra DPT [DISTANCE] 20 low cost": {
+    "image": "assets/builds/Cra DPT [DISTANCE] 20 low cost.png",
+    "url": "https://www.zenithwakfu.com/builder/5qe9e",
+    "keywords": ["Low Cost"],
+    "class": "Cra",
+    "description": "Fait par Shodan ",
+    "tranche": 20
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
