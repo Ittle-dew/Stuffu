7bc81bfe25daaec1c32a2ad4ec3c127971a35aff
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-06 13:56:28 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -1944,6 +1944,15 @@ const BUILD_DATA = {
     "description": "",
     "tranche": 245
   }
+,
+  "Xelor DPT [DISTANCE] 20 low cost": {
+    "image": "assets/builds/Xelor DPT [DISTANCE] 20 low cost.png",
+    "url": "https://www.zenithwakfu.com/builder/irzw0",
+    "keywords": ["Low Cost"],
+    "class": "Xelor",
+    "description": "Fait par Shodan ",
+    "tranche": 20
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
