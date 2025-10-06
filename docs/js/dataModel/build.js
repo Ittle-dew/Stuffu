f74ebbc2b386ab76e81bd3cf616806935cf0d6ed
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-06 16:27:16 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -2307,6 +2307,15 @@ const BUILD_DATA = {
     "description": "Fait par SHODAN",
     "tranche": 35
   }
+,
+  "Osamodas DPT [DISTANCE] 35 low cost": {
+    "image": "assets/builds/Osamodas DPT [DISTANCE] 35 low cost.png",
+    "url": "https://www.zenithwakfu.com/builder/zw2sp",
+    "keywords": ["Low Cost"],
+    "class": "Osamodas",
+    "description": "Fait par SHODAN",
+    "tranche": 35
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
