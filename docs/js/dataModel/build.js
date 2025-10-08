0120489ac41a0d9187adb4d97c5e176baabd57cf
Killian Lopez <Killian.Lopez@outlook.com>
2025-10-08 11:49:02 +0200

    Transfert Data

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -2389,6 +2389,15 @@ const BUILD_DATA = {
     "description": "Fait par Shodan !",
     "tranche": 50
   }
+,
+  "Zobal DPT [MELEE] 50 low cost": {
+    "image": "assets/builds/Zobal DPT [MELEE] 50 low cost.png",
+    "url": "https://www.zenithwakfu.com/builder/uohdw",
+    "keywords": ["Low Cost"],
+    "class": "Zobal",
+    "description": "Fait par Shodan !",
+    "tranche": 50
+  }
 };
 
 export { BUILD_DATA, CLASS_LIST };
