3cbc154eb2b603fccb880d14a31863c3a23f0feb
Stuffu Admin <stuffu@local>
2025-10-10 14:11:49 +0200

    chore(guides): update Iop_M DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -10,5 +10,14 @@ export const CLASS_GUIDES = [
     ],
     "subliGenerique": [":subliIII: Ambition", ":subliIII: Neutralité"],
     "subliLowCost": [":subliII: Consolation Critique", ":subliI: Puissance Brute"]
+  },
+  {
+    "name": "Iop",
+    "imgClasse": "Iop_M",
+    "key": "DPT",
+    "title": ":Iop: Iop DPT :Iop:",
+    "lines": [":subliEpique: Maniement 2 mains, Mesure III, Démesure, Mesure I", ":subliRelique: Petulance, Brasero, Stasification"],
+    "subliGenerique": [":subliIII: Poids plumes (8pm) :subliII: x2 Poids plumes :subliII: Retour PA :subliIII: Influence :subliIII: Jugement :subliIII: Embuscade :subliII: Retour PM"],
+    "subliLowCost": [":subliI: Influence :subliII: Embuscade :subliI: Poids plumes (viser mini 6pm et 4 PP I) :subliII: Influence Paradoxale"]
   }
 ];
