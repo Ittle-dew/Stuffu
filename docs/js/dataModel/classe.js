0545e009e7d63986a2c9110f6818b88ff171a7ef
Stuffu Admin <stuffu@local>
2025-10-13 01:50:36 +0200

    chore(guides): update Sacrieur_F DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -79,5 +79,15 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Inflexibilité [Build Maximum 10pa hors combat] :subliEpique:  Démesuré [Necessite un build axé sur la parade pour profiter de 100%cc sans condition", ":subliRelique: Dernier instant [Essayez d'avoir 8pw pour toujours avoir 6+ et profiter du maximum de DI de la subli] :subliRelique: Par-Delà la barrière  [Aucune Condition]"],
     "subliGenerique": [":SubliII: Profit :SubliIII: Sanguinolence :SubliII: Force vitale :SubliII: Agilité Vitale :SubliIII: Poids Plumes :SubliIII: Influence :SubliIII: Ravage :subliIII: Jugement"],
     "subliLowCost": [":SubliII: Poids Plumes :SubliII: Sanguinolence :SubliII: Influence Paradoxale"]
-  }
+  },
+  {
+		"name": "Sacrieur",
+		"imgClasse": "Sacrieur_F",
+		"key": "DPT",
+		"title": ":sacrieur: Sacrieur DPT :sacrieur:",
+		"lines": ["t"],
+		"subliGenerique": ["t"],
+		"subliLowCost": ["t"],
+    "tips": ["t"]
+	}
 ];
