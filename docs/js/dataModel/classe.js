3c675dd19050d41fe4f1bf30a2b4ad66329770e2
Stuffu Admin <stuffu@local>
2025-10-12 23:37:25 +0200

    chore(guides): update Cra_F SUPPORT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -1,5 +1,5 @@
 export const CLASS_GUIDES = [
-  {
+{
     "name": "Cra",
     "imgClasse": "Cra_M",
     "key": "DPT",
@@ -7,5 +7,14 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Démesuré [Nécessite un stuff spécifique axé sur la parade, avec des subli comme :SubliIII: Dérobade Continue\"]", ":subliEpique: Dénouement [Build Full Maitrise critique/sans secondaire pour profiter de sublimation comme :SubliIII: Ambition et :SubliIII: Neutralité]", ":subliEpique: Elementalisme [Build axé Tri-élément qui demande d'avoir la même maitrise dans 3 éléments]", ":subliEpique: Mesure III [Oblige à jouer 50%cc avec des :SubliIII: Influence Vitale et :SubliII: Influence du Wakfu]", ":subliRelique: Par dela la Barrière [Aucune Condition, Best in slot]", ":subliRelique: Hypermétropie [Demande de build un peu de PO]"],
     "subliGenerique": [":SubliII: Retour PA :SubliIII: Ravage :SubliIII: Tellurisme Secondaire :SubliIII: Influence :SubliII: Force Vitale :SubliII: Puissance Brute :SubliIII: Acribie :SubliIII: Verrouillage :subliII: Sauvegarde :subliIII: Jugement"],
     "subliLowCost": [":SubliII: Longueur :SubliI: Influence :SubliII: Influence Paradoxale :SubliI: Puissance Brute :SubliII: Acribie :subliII: Sauvegarde"]
+  },
+  {
+    "name": "Cra",
+    "imgClasse": "Cra_F",
+    "key": "SUPPORT",
+    "title": ":Cra: Cra SUPPORT :Cra:",
+    "lines": [":subliEpique: Pacte de Wakfu [Build 8pw]", ":subliEpique: Volonté de Fer [Build 2 po hors combat grâce au +1 po natif du crâ]", ":subliRelique: Modularité [Aucune Condition, Best in slot]", ":subliRelique: Pétulance [Regain de PA sur roulade ou mouvement via balise]"],
+    "subliGenerique": [":SubliII: Vélocité :SubliIII:Volonté Direct :SubliIII: Cicatrisation :SubliII: Force Vitale :SubliII: Agilité Vitale :SubliII: Profit"],
+    "subliLowCost": [":SubliII: Volonté Direct:SubliI: Vélocité"]
   }
 ];
