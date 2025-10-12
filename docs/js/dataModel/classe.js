bc047c8356ddb251c29319e3bcaaaa47a5592417
Stuffu Admin <stuffu@local>
2025-10-12 15:39:02 +0200

    chore(guides): update Cra_F DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -19,5 +19,14 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Maniement 2 mains, Mesure III, Démesure, Mesure I", ":subliRelique: Petulance, Brasero, Stasification"],
     "subliGenerique": [":subliIII: Poids plumes (8pm) :subliII: x2 Poids plumes :subliII: Retour PA :subliIII: Influence :subliIII: Jugement :subliIII: Embuscade :subliII: Retour PM"],
     "subliLowCost": [":subliI: Influence :subliII: Embuscade :subliI: Poids plumes (viser mini 6pm et 4 PP I) :subliII: Influence Paradoxale"]
+  },
+  {
+    "name": "Cra",
+    "imgClasse": "Cra_F",
+    "key": "DPT",
+    "title": ":Cra: Cra DPT :Cra:",
+    "lines": [":subliEpique: Démesuré [Nécessite un stuff spécifique axé sur la parade], Dénouement [Build Full Maitrise critique/sans secondaire], Elementalisme [Build axé Tri-élément qui demande d'avoir la même maitrise dans 3 éléments], Mesure III [Oblige à jouer 50%cc]", ":subliRelique:Par dela la Barrière [Aucune Condition, Best in slot], Hypermétropie [Demande de build un peu de PO]"],
+    "subliGenerique": [":subliIII: Influence, Acribie, Verrouillage, Ravage :subliII: Force Vitale, Retour PA, Sauvegarde"],
+    "subliLowCost": [":subliII: Longueur, Influence Paradoxale, Acribie, Sauvegarde :subliI: Puissance Brute"]
   }
 ];
