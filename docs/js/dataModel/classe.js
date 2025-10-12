50146cb980aa13548ed8e22ab295da5db0dfda51
Stuffu Admin <stuffu@local>
2025-10-12 23:36:35 +0200

    chore(guides): update Cra_M DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -1,3 +1,11 @@
 export const CLASS_GUIDES = [
-
+  {
+    "name": "Cra",
+    "imgClasse": "Cra_M",
+    "key": "DPT",
+    "title": ":Cra: Cra DPT :Cra:",
+    "lines": [":subliEpique: Démesuré [Nécessite un stuff spécifique axé sur la parade, avec des subli comme :SubliIII: Dérobade Continue\"]", ":subliEpique: Dénouement [Build Full Maitrise critique/sans secondaire pour profiter de sublimation comme :SubliIII: Ambition et :SubliIII: Neutralité]", ":subliEpique: Elementalisme [Build axé Tri-élément qui demande d'avoir la même maitrise dans 3 éléments]", ":subliEpique: Mesure III [Oblige à jouer 50%cc avec des :SubliIII: Influence Vitale et :SubliII: Influence du Wakfu]", ":subliRelique: Par dela la Barrière [Aucune Condition, Best in slot]", ":subliRelique: Hypermétropie [Demande de build un peu de PO]"],
+    "subliGenerique": [":SubliII: Retour PA :SubliIII: Ravage :SubliIII: Tellurisme Secondaire :SubliIII: Influence :SubliII: Force Vitale :SubliII: Puissance Brute :SubliIII: Acribie :SubliIII: Verrouillage :subliII: Sauvegarde :subliIII: Jugement"],
+    "subliLowCost": [":SubliII: Longueur :SubliI: Influence :SubliII: Influence Paradoxale :SubliI: Puissance Brute :SubliII: Acribie :subliII: Sauvegarde"]
+  }
 ];
