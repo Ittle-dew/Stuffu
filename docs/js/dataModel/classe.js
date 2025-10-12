7ed6bf0e5970a586063642ec14637d7401588b1c
Stuffu Admin <stuffu@local>
2025-10-12 15:46:22 +0200

    chore(guides): update Ecaflip_F SOIN

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -25,5 +25,14 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Dénouement [Build Full Maitrise critique/sans secondaire], Mesure III [Oblige à jouer 50%cc hors combat. Le passif de l'écaflip +20%cc proc au début du combat et ne brise pas la sublimation]", ":subliRelique:  Alternance II [Alterner les élements sans utiliser de cartes de tarot sinon ça casse :68325798:Alternance II], Calibrage I/II [Demande de s'adapter et de préparer un petit coup en mélée chaque tour pour profiter des bonus], Par dela la barrière [Aucune Condition spéciale]"],
     "subliGenerique": [":subliIII: Consolation Critique,  Influence, Neutralité, Ambition, Jugement :subliII: Force Vitale, Influence du Wakfu, Puissance Brute"],
     "subliLowCost": [":subliII: Consolation Critique, Influence Paradoxale :subliI: Puissance Brute"]
+  },
+  {
+    "name": "Ecaflip",
+    "imgClasse": "Ecaflip_F",
+    "key": "SOIN",
+    "title": ":Ecaflip: Ecaflip SOIN :Ecaflip:",
+    "lines": [":subliEpique: Abnégation, Précision Chirurgicale [Moins de Soin Réalisé mais un peu peu d'Armure Donné pour Chachacrifice, nécéssite de ne build que 1 de portée]", ":subliRelique: Cuirasse Singulière II [Le seul Shield de l'ecaflip provient de Chachacrifice, ça permet de démultiplier le bonus d'armure donné en une fois sans aucune contrepartie],  Distribution [Seulement si vous avez un 2eme soigneur dans votre équipe pour combler la perte de soin]"],
+    "subliGenerique": ["ATTENTION, :SubliIII: REVIGORATION ANNULE :subliRelique: DISTRIBUTION", ":subliII: Force Vitale :subliII: Vélocité :subliII: Tolérance :subliII: Agilité Vitale :subliII: Intégrité :subliIII: Cicatrisation :subliIII: Revigoration :subliIII:Expert des Coup Critique :subliIII: Diagnostic"],
+    "subliLowCost": [":subliI: Vélocité :subliIII: Nature :subliII: Revigoration :subliI: Expert des Coup Critique", "Tips: Toujours build :subliI: :subliII: :subliIII: Revigoration, même Niveau 1 si vous jouez \"Puces\". ça permet de monter considérablement votre génération de veine ! Ajouter :subliI: Expert des CC même niveau 1 permet d'avoir également une ligne de soin en plus et donc plus de veine !"]
   }
 ];
