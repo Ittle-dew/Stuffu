819ef3cc84fe7be25a196314e1f4f1ed2065fb86
Stuffu Admin <stuffu@local>
2025-10-12 16:18:42 +0200

    chore(guides): delete Enutrof_M SUPPORT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -7,14 +7,5 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Inflexibilité [Build Maximum 10pa hors combat] :subliEpique:  Démesuré [Necessite un build axé sur la parade pour profiter de 100%cc sans condition", ":subliRelique: Dernier instant [Essayez d'avoir 8pw pour toujours avoir 6+ et profiter du maximum de DI de la subli] :subliRelique: Par-Delà la barrière  [Aucune Condition]"],
     "subliGenerique": [":SubliII: Profit :SubliIII: Sanguinolence :SubliII: Force vitale :SubliII: Agilité Vitale :SubliIII: Poids Plumes :SubliIII: Influence :SubliIII: Ravage :subliIII: Jugement"],
     "subliLowCost": [":SubliII: Poids Plumes :SubliII: Sanguinolence :SubliII: Influence Paradoxale"]
-  },
-  {
-    "name": "Enutrof",
-    "imgClasse": "Enutrof_M",
-    "key": "SUPPORT",
-    "title": ":Enutrof: Enutrof SUPPORT :Enutrof:",
-    "lines": [":subliEpique: Pacte de Wakfu [Pour Maximiser le retrait PM, nécessite 8pw hors cbt] :subliEpique: Volonté de Fer [Pour Maximiser le retrait PM, nécessite 3po hors cbt] :subliEpique: :subliEpique: Abnégation [Permet d'avoir un petit soin non négligeable sur les bourses si vous jouez off-soin]", ":subliRelique: Robuste [Réduction de dégâts à distance] :subliRelique: Modularité [Gain de Résistance] :subliRelique: Sentinelle [+3po en début de tour avant de bouger] :subliRelique: Energie Ancestrale [Gain de Résistance]"],
-    "subliGenerique": [":SubliII: Profit :SubliIII: Volonté Direct :SubliII: Force vitale :SubliII: Agilité Vitale :SubliIII: Cicatrisation :SubliII: Intégrité :SubliII: Abandon [Build sans maitrise secondaire]"],
-    "subliLowCost": [":SubliII: Volonté Direct"]
   }
 ];
