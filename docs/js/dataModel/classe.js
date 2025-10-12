ca7b5f52d8a3aa13fea19eaed0ee6a485db867fd
Stuffu Admin <stuffu@local>
2025-10-12 16:18:33 +0200

    chore(guides): delete Cra_F DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -8,15 +8,7 @@ export const CLASS_GUIDES = [
     "subliGenerique": [":subliIII: Poids plumes (8pm) :subliII: x2 Poids plumes :subliII: Retour PA :subliIII: Influence :subliIII: Jugement :subliIII: Embuscade :subliII: Retour PM"],
     "subliLowCost": [":subliI: Influence :subliII: Embuscade :subliI: Poids plumes (viser mini 6pm et 4 PP I) :subliII: Influence Paradoxale"]
   },
-  {
-    "name": "Cra",
-    "imgClasse": "Cra_F",
-    "key": "DPT",
-    "title": ":Cra: Cra DPT :Cra:",
-    "lines": [":subliEpique: Démesuré [Nécessite un stuff spécifique axé sur la parade] :subliEpique: Dénouement [Build Full Maitrise critique/sans secondaire] :subliEpique: Elementalisme [Build axé Tri-élément] :subliEpique: Mesure III [Oblige à jouer 50%cc hors cbt]", ":subliRelique: Par dela la Barrière [Aucune Condition, Best in slot] :subliRelique: Hypermétropie [Demande de build un peu de PO]"],
-    "subliGenerique": [":SubliII: Retour PA :SubliIII: Ravage :SubliIII: Tellurisme Secondaire :SubliIII: Influence :SubliII: Force Vitale :SubliII: Puissance Brute :SubliIII: Acribie :SubliIII: Verrouillage :subliIII: Jugement :subliII: Sauvegarde"],
-    "subliLowCost": [":SubliII: Longueur :SubliI: Influence :SubliII: Influence Paradoxale :SubliI: Puissance Brute :SubliII: Acribie :subliII: Sauvegarde"]
-  },
+  
   
   
   
