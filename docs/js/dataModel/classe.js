3d831a92b43120a23cf26700b797dd4b113f8c96
Stuffu Admin <stuffu@local>
2025-10-12 15:49:55 +0200

    chore(guides): update Cra_F DPT

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -18,5 +18,14 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Abnégation, Précision Chirurgicale [Moins de Soin Réalisé mais un peu peu d'Armure Donné pour Chachacrifice, nécéssite de ne build que 1 de portée]", ":subliRelique: Cuirasse Singulière II [Le seul Shield de l'ecaflip provient de Chachacrifice, ça permet de démultiplier le bonus d'armure donné en une fois sans aucune contrepartie],  Distribution [Seulement si vous avez un 2eme soigneur dans votre équipe pour combler la perte de soin]"],
     "subliGenerique": ["ATTENTION, :SubliIII: REVIGORATION ANNULE :subliRelique: DISTRIBUTION", ":subliII: Force Vitale :subliII: Vélocité :subliII: Tolérance :subliII: Agilité Vitale :subliII: Intégrité :subliIII: Cicatrisation :subliIII: Revigoration :subliIII:Expert des Coup Critique :subliIII: Diagnostic"],
     "subliLowCost": [":subliI: Vélocité :subliIII: Nature :subliII: Revigoration :subliI: Expert des Coup Critique", "Tips: Toujours build :subliI: :subliII: :subliIII: Revigoration, même Niveau 1 si vous jouez \"Puces\". ça permet de monter considérablement votre génération de veine ! Ajouter :subliI: Expert des CC même niveau 1 permet d'avoir également une ligne de soin en plus et donc plus de veine !"]
+  },
+  {
+    "name": "Cra",
+    "imgClasse": "Cra_F",
+    "key": "DPT",
+    "title": ":Cra: Cra DPT :Cra:",
+    "lines": [":subliEpique: Démesuré [Nécessite un stuff spécifique axé sur la parade] :subliEpique: Dénouement [Build Full Maitrise critique/sans secondaire] :subliEpique:  Elementalisme [Build axé Tri-élément qui demande d'avoir la même maitrise dans 3 éléments] :subliEpique: Mesure III [Oblige à jouer 50%cc hors combat]"],
+    "subliGenerique": [":SubliII: Retour PA :SubliIII: Ravage :SubliIII: Tellurisme Secondaire :SubliIII: Influence :SubliII: Force Vitale :SubliII: Puissance Brute :SubliIII: Acribie :SubliIII: Verrouillage :subliIII: Jugement :subliII: Sauvegarde"],
+    "subliLowCost": ["Subli Low Cost: :SubliII: Longueur :SubliI: Influence :SubliII: Influence Paradoxale :SubliI: Puissance Brute :SubliII: Acribie :subliII: Sauvegarde"]
   }
 ];
