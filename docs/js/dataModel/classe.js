78e54c4957d35aa0e2c182c2cc464af3af07d0cb
Stuffu Admin <stuffu@local>
2025-10-12 15:55:15 +0200

    chore(guides): delete Ecaflip_F SOIN

--- docs/js/dataModel/classe.js
+++ docs/js/dataModel/classe.js
@@ -7,16 +7,5 @@ export const CLASS_GUIDES = [
     "lines": [":subliEpique: Maniement 2 mains, Mesure III, Démesure, Mesure I", ":subliRelique: Petulance, Brasero, Stasification"],
     "subliGenerique": [":subliIII: Poids plumes (8pm) :subliII: x2 Poids plumes :subliII: Retour PA :subliIII: Influence :subliIII: Jugement :subliIII: Embuscade :subliII: Retour PM"],
     "subliLowCost": [":subliI: Influence :subliII: Embuscade :subliI: Poids plumes (viser mini 6pm et 4 PP I) :subliII: Influence Paradoxale"]
-  },
-  
-  
-  {
-    "name": "Ecaflip",
-    "imgClasse": "Ecaflip_F",
-    "key": "SOIN",
-    "title": ":Ecaflip: Ecaflip SOIN :Ecaflip:",
-    "lines": [":subliEpique: Abnégation, Précision Chirurgicale [Moins de Soin Réalisé mais un peu peu d'Armure Donné pour Chachacrifice, nécéssite de ne build que 1 de portée]", ":subliRelique: Cuirasse Singulière II [Le seul Shield de l'ecaflip provient de Chachacrifice, ça permet de démultiplier le bonus d'armure donné en une fois sans aucune contrepartie],  Distribution [Seulement si vous avez un 2eme soigneur dans votre équipe pour combler la perte de soin]"],
-    "subliGenerique": ["ATTENTION, :SubliIII: REVIGORATION ANNULE :subliRelique: DISTRIBUTION", ":subliII: Force Vitale :subliII: Vélocité :subliII: Tolérance :subliII: Agilité Vitale :subliII: Intégrité :subliIII: Cicatrisation :subliIII: Revigoration :subliIII:Expert des Coup Critique :subliIII: Diagnostic"],
-    "subliLowCost": [":subliI: Vélocité :subliIII: Nature :subliII: Revigoration :subliI: Expert des Coup Critique", "Tips: Toujours build :subliI: :subliII: :subliIII: Revigoration, même Niveau 1 si vous jouez \"Puces\". ça permet de monter considérablement votre génération de veine ! Ajouter :subliI: Expert des CC même niveau 1 permet d'avoir également une ligne de soin en plus et donc plus de veine !"]
   }
 ];
